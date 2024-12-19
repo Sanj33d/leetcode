2cd6251a65997afd47296ce6dfdb8034ca7a5556
Sanj33d <144790238+Sanj33d@users.noreply.github.com>
2024-12-19 12:15:45 +0600

    Time: 4 ms (19.76%), Space: 21 MB (5.89%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -4,11 +4,11 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head == None: # bc1 (traversal ends, so return None)
+        if head == None: # 1. bc (traversal ends, so return None)
             return head 
-        elif head.val == val: # bc2 (val matched, so return the next node wo joining cur node)
+        elif head.val == val: # 3. operation (coincidentally rc too: val matched, so return the next node wo joining cur node)
             return self.removeElements(head.next, val)
-        else:                 # update (join the next node with cur node)
+        else:                 # 2. rc (traversal: join the next node with cur node)
             head.next = self.removeElements(head.next, val)
 
         return head
